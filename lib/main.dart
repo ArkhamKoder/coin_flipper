1d61fff5170b08c3f20fa460e2fe35a4ead4104a
mordecai <johnbasilogbhemhe@gmail.com>
2023-01-03 09:40:28 +0100

    used custom font from fonts.google

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,16 @@ void main() {
   return runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Coin Flip!', text)),
+        title: const Center(
+          child: Text(
+            'Coin Flip!',
+            style: TextStyle(
+              fontFamily: 'LuxuriousRoman',
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+            ),
+          ),
+        ),
         backgroundColor: Colors.yellow.shade200,
       ),
     ),
